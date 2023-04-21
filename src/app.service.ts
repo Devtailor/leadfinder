484a411eadd68c9bc71e4acf9b4ca659325c8828
jjkaarnavali <91603595+jjkaarnavali@users.noreply.github.com>
2023-04-21 18:00:57 +0300

    Parse LinkedIn URLs from sample-list file (#2)
    * LF add XLSX parser package, add method to parse linkedin urls from xlsx file
    
    * LF use import instead of require, use shorter for loop syntax

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as XLSX from 'xlsx';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getLinkedInUrls(): Map<number, string> {
+    const workbook = XLSX.readFile('src/data/sample-list.xlsx');
+    const [sheetName] = workbook.SheetNames;
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json(sheet);
+    const linkedinUrls = new Map<number, string>();
+    data
+      .slice(0, 10)
+      .forEach((row, i) =>
+        linkedinUrls.set(i, <string>(<Record<string, unknown>>row)['linkedin']),
+      );
+    return linkedinUrls;
   }
 }
