0271044b89b69f48501c9dbc2db7d9484c9b546f
Jan Johan Kaarnaväli <jan.kaarnavali@gmail.com>
2023-04-22 12:53:33 +0300

    Fix typo

--- src/services/open-ai.service.ts
+++ src/services/open-ai.service.ts
@@ -6,7 +6,7 @@ import { Config, GeneratedEmailsJson } from '../interfaces';
 
 @Injectable()
 export class OpenAiService {
-  private readonly MAX_REQURSION = 10;
+  private readonly MAX_RECURSION = 10;
   private readonly openAi: OpenAIApi;
 
   constructor(@Inject(configFactory.KEY) readonly config: Config) {
@@ -17,12 +17,12 @@ export class OpenAiService {
   async getEmails(
     prompts: Map<number, string>,
     dna: string,
-    reqursion?: number,
+    recursion?: number,
   ): Promise<Map<number, GeneratedEmailsJson>> {
-    if (!reqursion) reqursion = 1;
-    if (reqursion > this.MAX_REQURSION)
+    if (!recursion) recursion = 1;
+    if (recursion > this.MAX_RECURSION)
       throw new EmailGenerationFailedException();
-      
+
     const emails = new Map<number, GeneratedEmailsJson>();
     for (const prompt of prompts) {
       try {
@@ -35,8 +35,8 @@ export class OpenAiService {
         const json = JSON.parse(data.choices[0].text!);
         emails.set(prompt[0], json);
       } catch (error) {
-        reqursion++;
-        this.getEmails(prompts, dna, reqursion);
+        recursion++;
+        this.getEmails(prompts, dna, recursion);
       }
     }
     return emails;
