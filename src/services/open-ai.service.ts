ef490ddff298994887bf2fbaa419f11876657d1c
Jan Johan Kaarnaväli <jan.kaarnavali@gmail.com>
2023-04-22 12:42:14 +0300

    fix adding prompt to gpt request, add exception if email generation fails for too many times

--- src/services/open-ai.service.ts
+++ src/services/open-ai.service.ts
@@ -1,10 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Configuration, OpenAIApi } from 'openai';
+import { EmailGenerationFailedException } from 'src/exceptions';
 import { configFactory } from '../config.factory';
-import { Config } from '../interfaces';
+import { Config, GeneratedEmailsJson } from '../interfaces';
 
 @Injectable()
 export class OpenAiService {
+  private readonly MAX_REQURSION = 10;
   private readonly openAi: OpenAIApi;
 
   constructor(@Inject(configFactory.KEY) readonly config: Config) {
@@ -15,21 +17,26 @@ export class OpenAiService {
   async getEmails(
     prompts: Map<number, string>,
     dna: string,
-  ): Promise<Map<number, string>> {
-    const emails = new Map<number, string>();
+    reqursion?: number,
+  ): Promise<Map<number, GeneratedEmailsJson>> {
+    if (!reqursion) reqursion = 1;
+    if (reqursion > this.MAX_REQURSION)
+      throw new EmailGenerationFailedException();
+      
+    const emails = new Map<number, GeneratedEmailsJson>();
     for (const prompt of prompts) {
       try {
         const { data } = await this.openAi.createCompletion({
           model: 'text-davinci-003',
-          prompt,
+          prompt: prompt[1],
+          max_tokens: 2500,
         });
-        console.log(data.choices[0].text);
+
+        const json = JSON.parse(data.choices[0].text!);
+        emails.set(prompt[0], json);
       } catch (error) {
-        // TODO: Sth is wrong with prompt
-        Object.entries(error).forEach(([key, value]) =>
-          console.log(key, value),
-        );
-        throw error;
+        reqursion++;
+        this.getEmails(prompts, dna, reqursion);
       }
     }
     return emails;
